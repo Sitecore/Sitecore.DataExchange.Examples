06a0f35a39199556dcdedeb49cdcdd5c708caf83
Adam Conn <adc@sitecore.net>
2018-03-06 11:32:09 -0800

    added troubleshooter for file system provider

---
+++ data-exchange-framework-file-system-provider/Examples.DataExchange.Providers.FileSystem/TextFileEndpointTroubleshooter.cs
@@ -0,0 +1,38 @@
+﻿using Sitecore.DataExchange.Models;
+using Sitecore.DataExchange.Troubleshooters;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples.DataExchange.Providers.FileSystem
+{
+    public class TextFileEndpointTroubleshooter : BaseEndpointTroubleshooter
+    {
+        public TextFileEndpointTroubleshooter()
+        {
+        }
+        protected override ITroubleshooterResult Troubleshoot(Endpoint endpoint, TroubleshooterContext context)
+        {
+            var settings = endpoint.GetPlugin<TextFileSettings>();
+            if (settings == null)
+            {
+                return TroubleshooterResult.FailResult("The endpoint is missing the plugin TextFileSettings.");
+            }
+            if (string.IsNullOrWhiteSpace(settings.Path))
+            {
+                return TroubleshooterResult.FailResult("The endpoint does not have a file path specified on it.");
+            }
+            if (!File.Exists(settings.Path))
+            {
+                return TroubleshooterResult.FailResult(
+                    "The path specified on the endpoint either " + 
+                    "points to a file that does not exist " +
+                    "or the process does not have permission to read the file.");
+            }
+            return TroubleshooterResult.SuccessResult("The specified file exists and can be accessed.");
+        }
+    }
+}
